f489d613d42b6f2d6d2faa7e1d7f59ed39f6777b
Tharanish M <tharanishtharanish07@gmail.com>
2026-07-30 22:08:09 +0530

    Fix Render database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,16 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+    define: {
+      timestamps: true,
+    },
+  }
+);
 
 export const connectDB = async () => {
   try {
